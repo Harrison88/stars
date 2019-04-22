0adcd26e868600937496794437d77af26ddba01b
Harrison Morgan <harrison.morgan@gmail.com>
2019-04-22 14:07:58 -0500

    Add doc strings to find_font() and create_words_template()

--- templates.py
+++ templates.py
@@ -13,6 +13,11 @@ from typing import Union
 
 
 def find_font(query: str) -> Union[str, None]:
+    """Return the absolute path to a system font file.
+    
+    Arguments:
+    query -- the font file name (e.g., 'DejaVuSerif.ttf')
+    """
     fonts = findSystemFonts()
     for font_path in fonts:
         if font_path.endswith(query):
@@ -24,7 +29,17 @@ def create_words_template(message: str,
                           x_padding: int = 10,
                           y_padding: int = 10,
                           ) -> Image:
+    """Returns an Image containing white words on black background.
     
+    This Image is suitable for being passed to a Sky class as a template_image.
+    
+    Arguments:
+    message -- the words to be written
+    font -- the name of the font file to be loaded (default 'DejaVuSerif.ttf')
+    font_size -- the size of the font (default 100)
+    x_padding -- the number of pixels to the left and right of the words
+    y_padding -- the number of pixels above and below the words
+    """
     pil_font = ImageFont.truetype(find_font(font), font_size)
     x, y = pil_font.getsize(message)
     dimensions = (x + (x_padding * 2), y + (y_padding * 2))
