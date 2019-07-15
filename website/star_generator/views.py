7d8146cf6af29fa91312376d6690153e5f2bf1f5
Harrison Morgan <harrison.morgan@gmail.com>
2019-07-14 23:21:06 -0500

    Add support for all Sky parameters

--- website/star_generator/views.py
+++ website/star_generator/views.py
@@ -11,8 +11,15 @@ def index(request):
 
 def generate_stars(request):
     dimensions = (int(request.POST["pixels-x"]), int(request.POST["pixels-y"]))
-    starry_sky = Sky(dimensions=dimensions)
+    star_fraction = float(request.POST['star-fraction'])
+    star_quality = float(request.POST['star-quality'])
+    star_intensity = float(request.POST['star-intensity'])
+    star_tint_exp = float(request.POST['star-tint-exp'])
+    star_color = int(request.POST['star-color'])
+
+    starry_sky = Sky(dimensions, star_fraction, star_quality, star_intensity, star_tint_exp, star_color)
     starry_sky.generate_sky()
+
     bytes = BytesIO()
     starry_sky.image.save(bytes, "PNG")
     return HttpResponse(bytes.getvalue(), content_type="image/png")
