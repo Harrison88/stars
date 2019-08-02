5c761c10f782411af1a0812b599c13be1e053c40
Harrison Morgan <harrison.morgan@gmail.com>
2019-08-02 08:45:40 -0500

    Switch to KeenUI on Vue.js

--- website/star_generator/views.py
+++ website/star_generator/views.py
@@ -1,23 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 from .generator.stars import Sky
 from io import BytesIO
-
+import json
 
 def index(request):
     return render(request, "star_generator/index.html")
 
-
+@csrf_exempt
 def generate_stars(request):
-    dimensions = (int(request.POST["pixels-x"]), int(request.POST["pixels-y"]))
-    star_fraction = float(request.POST['star-fraction'])
-    star_quality = float(request.POST['star-quality'])
-    star_intensity = float(request.POST['star-intensity'])
-    star_tint_exp = float(request.POST['star-tint-exp'])
-    star_color = int(request.POST['star-color'])
+    values = json.loads(request.body)
+    dimensions = (values.pop("pixels_x"), values.pop("pixels_y"))
 
-    starry_sky = Sky(dimensions, star_fraction, star_quality, star_intensity, star_tint_exp, star_color)
+    starry_sky = Sky(dimensions, **values)
     starry_sky.generate_sky()
 
     bytes = BytesIO()
