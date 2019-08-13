a2172a048733b3185d6dc61cff51b5aff12af404
Harrison Morgan <harrison.morgan@gmail.com>
2019-08-13 12:33:58 -0500

    Updated settings for production

--- website/starsite/settings.py
+++ website/starsite/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "1&h96tu3h0v-ze=v*zrd1mu&bvkno-yll%si2!3(!apgb76hhb"
+with open(os.path.join(BASE_DIR, 'starsite', 'secret_key.txt')) as f:
+    print(__file__)
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["starsite-hm.appspot.com", "harrisonmorgan.dev", "www.harrisonmorgan.dev"]
 
