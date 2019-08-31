f0889120761387c8c96c3270b45bcab099705d93
Harrison Morgan <harrison.morgan@gmail.com>
2019-08-30 20:48:08 -0500

    Add a page to support templates

--- website/star_generator/urls.py
+++ website/star_generator/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("templates", views.templates, name="templates"),
     path("generate_stars", views.generate_stars, name="generate_stars"),
+    path("generate_stars_template", views.generate_stars_template, name="generate_stars_template"),
 ]
--- website/star_generator/views.py
+++ website/star_generator/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from .generator.stars import Sky
+from .generator.templates import create_words_template
 from io import BytesIO
 import json
 
@@ -21,5 +22,18 @@ def generate_stars(request):
     starry_sky.generate_sky()
 
     bytes = BytesIO()
+    starry_sky.image.save(bytes, "PNG")
+    return HttpResponse(bytes.getvalue(), content_type="image/png")
+
+@csrf_exempt
+def generate_stars_template(request):
+    values = json.loads(request.body)
+    message = values.pop("template_text")
+    font_size = int(values.pop("font_size"))
+    template = create_words_template(message, font_size=font_size)
+    starry_sky = Sky(dimensions=template.size, template_image=template, **values)
+    starry_sky.generate_sky()
+
+    bytes= BytesIO()
     starry_sky.image.save(bytes, "PNG")
     return HttpResponse(bytes.getvalue(), content_type="image/png")
